ea53124cdd1cb770486565f25f382c30827eb8d9
Travis j Barton <39603097+Travis-Barton@users.noreply.github.com>
2018-12-24 21:34:46 -0800

    Update Reports.py

--- Reports.py
+++ Reports.py
@@ -40,7 +40,7 @@ import warnings
 from datetime import datetime
 from datetime import timedelta
 import time
-
+import random
 reddit = praw.Reddit(user_agent='Comment Extraction (by /u/USERNAME)',
                      client_id='plLFnSdBy7b8ZQ', client_secret='_fv-EVVpz_m4iekd9a2EFsfJ66E',
                      username=base64.b64decode('UHJpdmF0ZUFza1NjaWVuY2VCb3Q='), 
@@ -48,6 +48,8 @@ reddit = praw.Reddit(user_agent='Comment Extraction (by /u/USERNAME)',
 
 travsbots = reddit.subreddit('travsbots')
 history = pd.read_csv(r'history.csv')
+
+history = history.iloc[:,1:]
 lastposts = history.shape[0]
 
 k= 0
@@ -55,9 +57,11 @@ while True:
     times  = datetime.now().weekday()
     if times == 0:
         history = pd.read_csv(r'history.csv')
+        history = history.iloc[:,1:]
+        row = random.sample(list(np.where(history.iloc[:,4] == 0)[0]), 2)
         numerofposts = (history.shape[0])
         reddit.subreddit('travsbots').submit('Private Askscience Bot for my weekly update, Sir!', selftext = 
-                      'I have been working hard to record the content of r/askscience and attempt to predict the tags of each post. My goal is to someday be a moderator there! They are not currently allowing robots to be moderators, but I will be attempting to prove to them that I am reliable! \n \n This week, I have classified {} posts, and am doing more everyday. In general, I have an accuracy around %{} and have classified {} posts in total. I use a natural language proccessing technique, a variable reduction method invented by Travis Barton called Feed Networks and SVM in order to decide where each post belongs. You can read all about it under Passion Projects on his website: [www.wbbpredictions.com](http://www.wbbpredictions.com)'.format(history.shape[0]-lastposts, np.round(sum(history['correct'])/history.shape[0]*100, 2), history.shape[0])                       
+                      'I have been working hard to record the content of r/askscience and attempt to predict the tags of each post. My goal is to someday be a moderator there! They are not currently allowing robots to be moderators, but I will be attempting to prove to them that I am reliable! I try to predict the main 6 catagories, and leave the less populus ones to be classified as \'other\' but that might change as I gather more data! \n \n This week, I have classified {} posts, and am doing more everyday. In general, I have an accuracy around %{} and have classified {} posts in total. I use a natural language proccessing technique, a variable reduction method invented by Travis Barton called Feed Networks and SVM in order to decide where each post belongs. You can read all about it under Passion Projects on his website: [www.wbbpredictions.com](http://www.wbbpredictions.com) \n \n \n Some examples of trouble posts are: \n \n \"{}\" classified as {} when the mods classified it as {} \n \n and \n \n \"{}\" classified as {} when the mods classified it as {}'.format(history.shape[0]-lastposts, np.round(sum(history['correct'])/history.shape[0]*100, 2), history.shape[0], history.iloc[row[0], 1], history.loc[row[0], 'prediction'], history.loc[row[0], 'actual'], history.iloc[row[1], 1], history.loc[row[1], 'prediction'], history.loc[row[1], 'actual'])                       
                       )
         lastposts = history.shape[0]
         time.sleep(86400)
